5376cce339d6ba268af70997668d1c723537da89
Rynare <rynare.of.legend@gmail.com>
2024-10-25 18:40:05 +0700

    memperbaiki tampilan page service

--- pages/services/index.vue
+++ pages/services/index.vue
@@ -5,37 +5,56 @@ useSeoMeta({
 const config = useRuntimeConfig();
 const {
   data: services,
-  pending,
-  error,
-  refresh,
+  error: errorSevices,
+  refresh: refreshServices,
 } = await useFetch(`${config.public.apiPublic}/api/services`, {
   pick: ["data"],
   key: "services-data",
 });
+
+const {
+  status,
+  data: staticData,
+  error,
+  refresh,
+} = await useFetch(`${config.public.apiPublic}/api/settings`, {
+  pick: ["data"],
+  key: "res-static-data",
+  server: true,
+});
 </script>
 <template>
   <section id="services">
-    <div class="p-6 px-10 mx-auto">
-      <h1
-        class="pb-2 my-4 text-4xl font-bold border-b-4 text-main w-fit border-main"
-        data-aos="fade-down"
+    <div class="py-6 mx-auto">
+      <div
+        class="title"
+        :style="`background-image: url(${config.public.apiPublic}/images/static/${staticData.data.service_hero_image});`"
       >
-        Layanan Kami
-      </h1>
-      <p
-        class="mb-8 text-sm font-normal text-justify"
-        data-aos="fade-down"
-        data-aos-delay="100"
-      >
-        PT Bina Jaya Perkasa Mandiri menawarkan berbagai layanan unggulan yang
-        dirancang untuk memenuhi kebutuhan pembangunan dan konstruksi di
-        Indonesia. Dengan pengalaman lebih dari 30 tahun, kami berkomitmen untuk
-        memberikan layanan yang cepat, tepat, dan berkualitas.
-      </p>
+        <div class="bg-[#ffffffb3] p-10 px-4 tablet:px-10">
+          <h1
+            class="pb-2 my-4 text-4xl font-bold capitalize border-b-4 text-main w-fit border-main aos-init aos-animate"
+            data-aos="fade-up"
+          >
+            {{ staticData.data.service_hero_title }}
+          </h1>
+          <div
+            class="mb-8 text-sm font-normal text-justify aos-init aos-animate"
+            data-aos="fade-up"
+          >
+            PT Bina Jaya Perkasa Mandiri menawarkan berbagai layanan unggulan
+            yang dirancang untuk memenuhi kebutuhan pembangunan dan konstruksi
+            di Indonesia. Dengan pengalaman lebih dari 30 tahun, kami
+            berkomitmen untuk memberikan layanan yang cepat, tepat, dan
+            berkualitas.
+          </div>
+        </div>
+      </div>
 
-      <div class="flex flex-row flex-wrap gap-6 md:flex-col">
+      <div
+        class="flex flex-row flex-wrap gap-6 p-10 px-4 tablet:px-10 md:flex-col"
+      >
         <div
-          class="flex flex-col md:flex-row shadow-md overflow-hidden services-card md:w-full w-[40%] grow"
+          class="flex flex-col w-full overflow-hidden shadow-md md:flex-row services-card"
           data-aos="fade-down"
           v-for="(service, index) in services.data"
           :key="index"
